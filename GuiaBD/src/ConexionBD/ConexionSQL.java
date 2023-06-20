109cf5e16409f7d6cb3dddb041538087b673c83c
BlinkDany <111986563+BlinkDany@users.noreply.github.com>
2023-06-20 08:26:31 -0500

    Add files via upload

---
+++ GuiaBD/src/ConexionBD/ConexionSQL.java
@@ -0,0 +1,74 @@
+package ConexionBD;
+
+import java.sql.Connection;
+import oracle.jdbc.pool.OracleDataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConexionSQL {
+
+    String URL = "jdbc:postgresql://localhost:5432/MVC";
+    private final String USER = "postgres";
+    private final String PASSWORD = "1234";
+
+    private Connection con = null;
+    private Statement st;
+
+    public ConexionSQL() {
+        /*
+        con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexionSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         */
+
+        try {
+            OracleDataSource ods = new OracleDataSource();
+            ods.setURL("jdbc:oracle:thin:@localhost:1521:XE");
+            ods.setUser("SYSTEM");
+            ods.setPassword("1234");
+            con = ods.getConnection();
+            System.out.println("conectado");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "¡Error de conexión!", null, JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public boolean CRUD(String sql) {
+        st = null;
+        try {
+            st = con.createStatement();
+            st.execute(sql);
+            st.close();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionSQL.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public ResultSet Consultas(String SQL) {
+
+        st = null;
+        try {
+            st = con.createStatement();
+            ResultSet rs = st.executeQuery(SQL);
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
---
+++ GuiaBD/src/guiabd/GuiaBD.java
@@ -0,0 +1,13 @@
+package guiabd;
+
+import ConexionBD.ConexionSQL;
+
+public class GuiaBD {
+
+    public static void main(String[] args) {
+        
+        ConexionBD.ConexionSQL n = new ConexionSQL();
+        
+    }
+    
+}
